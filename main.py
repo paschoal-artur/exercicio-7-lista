5fbd3a79d8b544f725dc5ab148da62973ba565e3
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-04-01 23:35:11 -0300

    added code

---
+++ main.py
@@ -0,0 +1,9 @@
+#Receiving value
+side = float(input("Type the value of the square side: \n"))
+
+#Calculating the area
+area = side**2
+
+#Printing the double of the area
+area_double = area * 2
+print(f"The double of the square area is {area_double}")
